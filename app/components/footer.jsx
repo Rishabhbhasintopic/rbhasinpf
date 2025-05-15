49a01683d382708b329088703bf4031c70081a30
rishabhbhasintopic <rishabhbhasin7777@gmail.com>
2025-05-15 14:02:04 +0530

    experiences updatedhh

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,9 +12,9 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/abu-said-bd/" className="text-[#16f2b3]">Abu Said</Link>
+            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/rishabh-bhasin-44081a179/" className="text-[#16f2b3]">Rishabh Bhasin</Link>
           </p>
-          <div className="flex items-center gap-5">
+          {/* <div className="flex items-center gap-5">
             <Link
               target="_blank"
               href="https://github.com/said7388/developer-portfolio"
@@ -31,7 +31,7 @@ function Footer() {
               <CgGitFork />
               <span>Fork</span>
             </Link>
-          </div>
+          </div> */}
         </div>
       </div>
     </div >
